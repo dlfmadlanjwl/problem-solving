f0333609a468ad56686d732f3a42d6ea8f7f1e4e
JunhyeokParkOfficial <111430281+JunhyeokParkOfficial@users.noreply.github.com>
2023-09-14 17:21:46 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 152 ms, Memory: 15904 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = i+1;
+        }
+
+        for(int i=0;i<m;i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken())-1;
+            int b = Integer.parseInt(st.nextToken())-1;
+
+            int tmp;
+            for(int j=0;j<=(b-a)/2;j++){
+                tmp = arr[a+j];
+                arr[a+j] = arr[b-j];
+                arr[b-j] = tmp;
+            }
+        }
+        for(int j=0;j<n;j++){
+            System.out.print(arr[j]+" ");
+        }
+    }
+}
