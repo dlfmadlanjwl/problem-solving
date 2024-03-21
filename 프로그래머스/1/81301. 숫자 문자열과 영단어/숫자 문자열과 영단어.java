e0d0567e357828084bfad145a6398decb0f317aa
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2024-03-21 10:55:26 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static int solution(String s) {
+        String[] arr = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+
+        for(int i=0;i<arr.length;i++){
+            s = s.replace(arr[i],String.valueOf(i));
+        }
+        return Integer.parseInt(s);
+    }
+}
