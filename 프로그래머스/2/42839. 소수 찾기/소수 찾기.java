75da7d195598ee3b94d204275b5ca2d897b4c104
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2024-04-01 10:00:25 +0900

    [level 2] Title: 소수 찾기, Time: 3.27 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -5,33 +5,31 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Solution {
-    private boolean isPrime(int n) {
-        if (n <= 1) return false;
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) return false;
+    public int solution(String nums) {
+        List<Integer> numbers = nums.chars().map(c->c-'0').boxed().collect(Collectors.toList());
+
+        return getPrimes(0, numbers).size();
+    }
+
+    private boolean isPrime(int x){
+        if(x <= 1) return false;
+        for(int i=2; i * i <= x; i++){
+            if(x % i == 0) return false;
         }
         return true;
     }
 
-    private Set<Integer> getPrimes(int acc, List<Integer> numbers) {
+    private Set<Integer> getPrimes(int acc, List<Integer> numbers){
         Set<Integer> primes = new HashSet<>();
-        if (isPrime(acc)) primes.add(acc);
+        if(isPrime(acc)) primes.add(acc);
 
-        for (int i = 0; i < numbers.size(); i++) {
-            int nextAcc = acc * 10 + numbers.get(i);
+        for(int i=0; i < numbers.size(); i++){
+            int nextAcc = 10 * acc + numbers.get(i);
             List<Integer> nextNumbers = new ArrayList<>(numbers);
             nextNumbers.remove(i);
             primes.addAll(getPrimes(nextAcc, nextNumbers));
         }
 
         return primes;
-    }
-
-    public int solution(String nums) {
-        List<Integer> numbers = nums.chars()
-                .map(c -> c - '0')
-                .boxed()
-                .collect(Collectors.toList());
-        return getPrimes(0, numbers).size();
     }
 }
