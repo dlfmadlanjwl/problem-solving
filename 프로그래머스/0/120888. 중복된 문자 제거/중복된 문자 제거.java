6957edc138c90d759731ccae029ee65a508fa17a
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2024-04-08 21:22:09 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.18 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        Set<Character> set = new HashSet<>();
+        for(char c:my_string.toCharArray()){
+            if(!set.contains(c)) sb.append(c);
+            set.add(c);
+        }
+        
+        return sb.toString();
+    }
+}
