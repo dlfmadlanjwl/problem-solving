22d69911a78df2f8ec5426e30507c661c0d467f2
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2024-07-16 22:18:53 +0900

    [level 2] Title: 구명보트, Time: 17.31 ms, Memory: 56.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int count = 0;
+        int left = 0;
+        int right = people.length-1;
+        while(left<right){
+            if(people[left] + people[right] <= limit){
+                left++;
+            }
+            right--;
+            count++;
+        }
+        if(left==right) count++;
+        
+        return count;
+    }
+}
