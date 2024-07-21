b9aeeb387006d472136a71f58c7363fafcd24a88
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2024-07-21 14:01:34 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 444 ms, Memory: 78,624 KB -BaekjoonHub

---
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/README.md
@@ -0,0 +1,15 @@
+# [D3] [S/W 문제해결 기본] 8일차 - 암호문3 - 1230 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14zIwqAHwCFAYD) 
+
+### 성능 요약
+
+메모리: 78,624 KB, 시간: 444 ms, 코드길이: 2,017 Bytes
+
+### 제출 일자
+
+2024-07-21 14:01
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+
+		Scanner sc = new Scanner(System.in);
+
+		for(int test_case = 1; test_case <= 10; test_case++)
+		{
+            List<Integer> list = new ArrayList<>();
+        	int cipherLen = sc.nextInt();
+            for(int i=0;i<cipherLen;i++){
+            	list.add(sc.nextInt());
+            }
+            int operationLen = sc.nextInt();
+            for(int i=0;i<operationLen;i++){
+            	String op = sc.next();
+                if(op.equals("I")){
+                	int index = sc.nextInt();
+                	int itemCount = sc.nextInt();
+                	int[] items = new int[itemCount];
+                	for(int j=0;j<itemCount;j++){
+                		items[j] = sc.nextInt();
+                    }
+                    insert(index, items, list);
+                } else if(op.equals("D")){
+                	int index = sc.nextInt();
+                    int count = sc.nextInt();
+                    delete(index, count, list);
+                } else if(op.equals("A")){
+                	int itemCount = sc.nextInt();
+                    int[] items = new int[itemCount];
+                    for(int j=0;j<itemCount;j++){
+                    	items[j] = sc.nextInt();
+                    }
+                    append(items, list);
+                }
+            }   
+            System.out.print("#"+test_case+" ");
+            for(int i=0;i<10;i++){
+            	System.out.print(list.get(i)+" ");
+            }
+            System.out.println();
+        }
+	}
+    static void insert(int index, int[] items, List<Integer>list){
+    	for(int i=0; i<items.length; i++){
+        	list.add(i + index, items[i]);
+        }
+    }
+    static void delete(int index, int count, List<Integer>list){
+       	for(int i=0;i<count;i++){
+        	list.remove(index);
+        }
+    }
+    
+    static void append(int[] items, List<Integer> list){
+    	Arrays.stream(items).forEach(x -> list.add(x));
+    }
+}
