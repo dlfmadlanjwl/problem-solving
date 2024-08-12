4feb7b49e79b515c3b7177c27834c19063d4e771
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2024-08-12 13:46:25 +0900

    [level 3] Title: 거스름돈, Time: 15.32 ms, Memory: 52.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n, int[] money) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        
+        for(int m : money){
+            for(int i=m;i<=n;i++){
+                if(i-m >= 0){
+                    dp[i] = dp[i] + dp[i-m];
+                }
+            }
+        }
+        return dp[n];
+    }
+}
