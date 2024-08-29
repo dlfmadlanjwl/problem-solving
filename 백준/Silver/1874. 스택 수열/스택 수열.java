bf6a6d251d3d8060fdf82e8f10668d48d4edbd37
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2024-08-29 14:36:56 +0900

    [Silver II] Title: 스택 수열, Time: 300 ms, Memory: 27688 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -11,38 +11,23 @@ public class Main{
         int n = Integer.parseInt(br.readLine());
         Stack<Integer> stack = new Stack<>();
         int x = 1;
-        int count = 0;
         for(int i=0;i<n;i++){
             int input = Integer.parseInt(br.readLine());
-            if(stack.isEmpty()){
-                for(int j=x;j<=input;j++){
-                    stack.push(j);
-                    sb.append("+\n");
-                    x++;
-                }
+
+            for(;input >= x;x++){
+                stack.push(x);
+                sb.append("+").append("\n");
             }
-            while(!stack.isEmpty()){
-                if(stack.peek() > input){
-                    stack.pop();
-                    sb.append("-\n");
-                } else if(stack.peek() < input){
-                    if(x > n) {
-                        System.out.println("NO");
-                        return;
-                    }
-                    stack.push(x);
-                    sb.append("+\n");
-                    x++;
-                } else {
-                    stack.pop();
-                    sb.append("-\n");
-                    count++;
-                    break;
-                }
+
+            if(stack.peek() == input){
+                stack.pop();
+                sb.append("-").append("\n");
+            } else {
+                System.out.print("NO");
+                return;
             }
         }
 
-        if(count != n)System.out.print("NO");
-        else System.out.println(sb);
+        System.out.println(sb);
     }
 }
