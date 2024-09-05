d32a2788b95c0afa899a1ae5db2588b2d0d81a0c
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2024-09-05 10:30:10 +0900

    [D3] Title: 다양성 측정, Time: 144 ms, Memory: 20,424 KB -BaekjoonHub

---
+++ SWEA/D3/7728. 다양성 측정/README.md
@@ -0,0 +1,15 @@
+# [D3] 다양성 측정 - 7728 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWq40NEKLyADFARG) 
+
+### 성능 요약
+
+메모리: 20,424 KB, 시간: 144 ms, 코드길이: 652 Bytes
+
+### 제출 일자
+
+2024-09-05 10:30
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/7728. 다양성 측정/다양성 측정.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			boolean[] isVisited = new boolean[10];
+            String input = sc.next();
+            for(int i=0;i<input.length();i++){
+            	isVisited[input.charAt(i) - '0'] = true;
+            }
+            int answer = 0;
+            for(int i=0;i<10;i++){
+            	if(isVisited[i]) answer++;
+            }
+            
+            System.out.println("#"+test_case+" "+answer);
+		}
+	}
+}
