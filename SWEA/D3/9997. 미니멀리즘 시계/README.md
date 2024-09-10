882d765f26958c5ed9d24cc1f35de223b3a3dd98
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2024-09-10 16:06:53 +0900

    [D3] Title: 미니멀리즘 시계, Time: 192 ms, Memory: 25,084 KB -BaekjoonHub

---
+++ SWEA/D3/9997. 미니멀리즘 시계/README.md
@@ -0,0 +1,15 @@
+# [D3] 미니멀리즘 시계 - 9997 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXIvNBzKapEDFAXR) 
+
+### 성능 요약
+
+메모리: 25,084 KB, 시간: 192 ms, 코드길이: 413 Bytes
+
+### 제출 일자
+
+2024-09-10 16:06
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/9997. 미니멀리즘 시계/미니멀리즘 시계.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int x = sc.nextInt();
+            int h = x / 30;
+            int m = x % 30 * 2;
+            System.out.println("#"+test_case+" "+h+" "+m);
+		}
+	}
+}
