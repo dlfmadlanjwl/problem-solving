6313b5ec4d7d05f1f389907ce792c5f57d9d5138
dlfmadlanjwl <111430281+dlfmadlanjwl@users.noreply.github.com>
2024-11-18 15:12:02 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 149 ms, Memory: 21,156 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -1,36 +1,40 @@
 import java.util.*;
-import java.io.FileInputStream;
 
 class Solution
 {
 	public static void main(String args[]) throws Exception
 	{
 		Scanner sc = new Scanner(System.in);
-		
+
 		for(int test_case = 1; test_case <= 10; test_case++)
 		{
-        	sc.nextInt();
-            Queue<Integer> q = new LinkedList<>();
+            sc.nextInt();
+			int[] arr = new int[8];
             for(int i=0;i<8;i++){
-            	q.add(sc.nextInt());
+            	arr[i] = sc.nextInt();
             }
             
-            int n = 1;
+            int idx = 0;
+            int x = 1;
             while(true){
-                if(n>5) n=1;
-            	int x = q.poll();
-                if(x - n <= 0) {
-                    q.add(0);
-                    break;
+            	if(x > 5) x = 1;
+                if(idx > 7) idx = 0;
+                arr[idx] -= x;
+                if(arr[idx] <= 0){
+                    arr[idx] = 0;
+                	break;
                 }
-                q.add(x - n);
-                n++;
+                idx++;
+                x++;
             }
-            System.out.print("#"+test_case+" ");
-            for(int i=0;i<8;i++){
-            	System.out.print(q.poll()+" ");
+            System.out.print("#"+test_case+" "); 
+            for(int i=idx+1;i<arr.length;i++){
+            	System.out.print(arr[i]+" ");
+            }
+            for(int i=0;i<=idx;i++){
+            	System.out.print(arr[i]+" ");
             }
             System.out.println();
-        }
+		}
 	}
 }
